2806306f84ff299b8a504ecc7d2a264ee69e45be
dasiiphone92-prog <dasiiphone92@gmail.com>
2025-12-17 19:18:11 +0200

    Update api.js

--- front/src/api.js
+++ front/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://bcrypt-login-f954.onrender.com",
 });
 
 // מוסיף אוטומטית את הטוקן לכל בקשה
